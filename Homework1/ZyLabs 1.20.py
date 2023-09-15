bd21958a72df0907963c06f5a812ef82a39be506
Will Buckner Jr <143214947+WillBucknerJr@users.noreply.github.com>
2023-09-14 22:50:31 -0500

    Commit ZyLabs 1.20

---
+++ Homework1/ZyLabs 1.20.py
@@ -0,0 +1,12 @@
+#Will Buckner Jr#
+#PSID: 2101260#
+user_num1 = int(input("Enter integer:"))
+
+print("\nYou entered:", user_num1)
+print(user_num1, "squared is", (user_num1 ** 2))
+print("And", user_num1, "cubed is", (user_num1 ** 3), "!!")
+
+user_num2 = int(input("Enter another integer:"))
+
+print(f"\n{user_num1} + {user_num2} is {(user_num1 + user_num2)}")
+print(f"{user_num1} * {user_num2} is {(user_num1 * user_num2)}")
