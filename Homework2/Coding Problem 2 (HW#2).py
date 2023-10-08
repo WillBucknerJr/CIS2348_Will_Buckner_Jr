524efe04013aa7731ce8a0d3c680d0faac9c808a
Will Buckner Jr <143214947+WillBucknerJr@users.noreply.github.com>
2023-10-08 00:24:50 -0500

    Committing Coding Problem 2 Part C

--- Homework2/Coding Problem 2 (HW#2).py
+++ Homework2/Coding Problem 2 (HW#2).py
@@ -13,31 +13,34 @@ def is_valid_date(date_string):
 
 file_name = input()
 my_file = open(file_name, "r")
+output_file = open("parsedDates.tx", "w")
 zero = 0
 reading = my_file.readlines()
 
-for x in reading:
-    if "\n" in x:
-        replacement = x.replace("\n", "")
-        reading[zero] = replacement
-        zero += 1
-    else:
-        zero += 1
+with my_file, output_file:
+    for x in reading:
+        if "\n" in x:
+            replacement = x.replace("\n", "")
+            reading[zero] = replacement
+            zero += 1
+        else:
+            zero += 1
 
-for xy in reading:
-    date_str = xy
-    if date_str == '-1':
-        break
-    # Extract Date #
-    date_start = date_str.find(" ")
-    date_end = date_str.find(',', date_start)
-    if date_start != -1 and date_end != -1:
-        date = date_str[date_start + 1:date_end]
+    for xy in reading:
+        date_str = xy
+        if date_str == '-1':
+            break
+        # Extract Date #
+        date_start = date_str.find(" ")
+        date_end = date_str.find(',', date_start)
+        if date_start != -1 and date_end != -1:
+            date = date_str[date_start + 1:date_end]
 
-        if is_valid_date(date_str):
-            formatted_date = datetime.strptime(date_str, '%B %d, %Y').strftime('%m/%d/%Y')
-            print(formatted_date)
+            if is_valid_date(date_str):
+                formatted_date = datetime.strptime(date_str, '%B %d, %Y').strftime('%m/%d/%Y')
+                print(formatted_date)
+                output_file.write(f'{formatted_date}\n')
+            else:
+                continue
         else:
             continue
-    else:
-        continue
